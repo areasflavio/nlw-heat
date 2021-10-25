b984df1d626c87c03db87e1fe4d461d2a7dbbf4f
Flávio Arêas <vflavio9@gmail.com>
2021-10-25 09:09:13 -0300

    chore: connect to socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import 'dotenv/config';
 
 import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
 
 import { router } from './routes';
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado no Socket:${socket.id}`);
+});
 
 app.use(express.json());
 app.use(router);
@@ -23,4 +38,4 @@ app.get('/signin/callback', (req, res) => {
   return res.json({ code });
 });
 
-app.listen(3333, () => console.log('💻 Server is up on port: 3333'));
+export { serverHttp, io };
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import { serverHttp } from './app';
+
+serverHttp.listen(3333, () => console.log('💻 Server is up on port: 3333'));
